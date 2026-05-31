5623443ccc33a2451d208a9c5e21a1d4422bf183
bahaamehyeldine <mehyeldinebahaa@gmail.com>
2026-05-31 16:16:46 +0300

    fix feature names warning in model prediction

--- api/model.py
+++ api/model.py
@@ -1,5 +1,5 @@
 import joblib
-import numpy as np
+import pandas as pd
 from pathlib import Path
 from api.schema import SalaryInput
 
@@ -7,13 +7,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 model = joblib.load(BASE_DIR / "models" / "decision_tree_model.pkl")
 
 def predict_salary(input_data: SalaryInput) -> float:
-    features = np.array([[
+    features = pd.DataFrame([[
         input_data.work_year,
         input_data.experience_level,
         input_data.job_title,
         input_data.remote_ratio,
         input_data.company_location,
         input_data.company_size
-    ]])
+    ]], columns=['work_year', 'experience_level', 'job_title', 'remote_ratio', 'company_location', 'company_size'])
     prediction = model.predict(features)
     return float(prediction[0])
